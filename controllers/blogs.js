17331f09123f1ffbaaa76f5432fc8cc060ff6279
Erno Jokela <38183216+Ejo0@users.noreply.github.com>
2022-05-01 09:38:18 +0300

    13.13: add search from blog title

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
-const { SECRET_KEY } = require('../util/config')
 const router = require('express').Router()
+const { Op } = require('sequelize')
+const { SECRET_KEY } = require('../util/config')
 const { Blog, User } = require('../models')
 
 const tokenExtractor = (req, res, next) => {
@@ -19,11 +20,20 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
+
   const blogs = await Blog.findAll({
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
   res.json(blogs)
 })
