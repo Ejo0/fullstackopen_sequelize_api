84307f537b4291c9e065b9567ddabdb16a0e7550
Erno Jokela <38183216+Ejo0@users.noreply.github.com>
2022-05-01 10:22:42 +0300

    13.16: add route api/authors

---
+++ controllers/authors.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+const { fn, col } = require('sequelize')
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [fn('COUNT', col('id')), 'blogs'],
+      [fn('SUM', col('likes')), 'likes']
+    ],
+    group: 'author'
+  })
+  res.json(authors)
+})
+
+module.exports = router
